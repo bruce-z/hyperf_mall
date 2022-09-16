221d016dd4459f54bb0037baddf1e92c1ab47b7b
bruce.zhang <yunguang.zhang@71360.com>
2022-09-16 11:44:33 +0800

    fix 添加活动服务管理

--- install/config.py
+++ install/config.py
@@ -29,56 +29,63 @@ service_project_dir = "/data/www"
 services = [
     {
         'git_name': 'hypref_mall_goods',  # git仓库名字 不可修改 -- 商品服务
-        'service_name': 'hypref_mall_goods',  # 服务名，默认与仓库名保持一致， 可自定义
+        'service_name': 'hyperf_mall_goods',  # 服务名，默认与仓库名保持一致， 可自定义
         'ch_name': '商品服务',
         'port': 5503,  # 启用端口 默认 本地与 docker启动 端口一致，可自定义
         'db_name': 'goods',  # 数据库名字
     },
     {
         'git_name': 'hypref_mall_images',  # git仓库名字 不可修改 -- 图片服务
-        'service_name': 'hypref_mall_images',
+        'service_name': 'hyperf_mall_images',
         'ch_name': '图片服务',
         'port': 5504,
         'db_name': 'file_center',
     },
     {
         'git_name': 'hypref_mall_member',  # git仓库名字 不可修改 -- 会员服务
-        'service_name': 'hypref_mall_member',
+        'service_name': 'hyperf_mall_member',
         'ch_name': '会员服务',
         'port': 5505,
         'db_name': 'member',
     },
     {
         'git_name': 'hypref_mall_config',  # git仓库名字 不可修改 -- 配置服务
-        'service_name': 'hypref_mall_config',
+        'service_name': 'hyperf_mall_config',
         'ch_name': '配置服务',
         'port': 5506,
         'db_name': 'config',
     },
     {
         'git_name': 'hypref_mall_order',  # git仓库名字 不可修改 -- 订单服务
-        'service_name': 'hypref_mall_order',
+        'service_name': 'hyperf_mall_order',
         'ch_name': '订单服务',
         'port': 5507,
         'db_name': 'orders',
     },
     {
         'git_name': 'hypref_mall_pay',  # git仓库名字 不可修改 -- 支付服务
-        'service_name': 'hypref_mall_pay',
+        'service_name': 'hyperf_mall_pay',
         'ch_name': '支付服务',
         'port': 5508,
         'db_name': False,
     },
     {
+        'git_name': 'hyperf_mall_activity',  # git仓库名字 不可修改 -- 支付服务
+        'service_name': 'hyperf_mall_activity',
+        'ch_name': '活动服务',
+        'port': 5509,
+        'db_name': False,
+    },
+    {
         'git_name': 'hypref_mall_admin',  # git仓库名字 不可修改 -- 路由服务
-        'service_name': 'hypref_mall_admin',
+        'service_name': 'hyperf_mall_admin',
         'ch_name': '路由服务',
         'port': 5566,
         'db_name': False,
     },
     {
         'git_name': 'hypref_mall_font',  # git仓库名字 不可修改 -- 后台web站点
-        'service_name': 'hypref_mall_font',
+        'service_name': 'hyperf_mall_font',  # 名字被占用，如需修改，需要修改main.py中对应的值
         'ch_name': '后台web_admin',
         'port': 5569,
         'db_name': False,
@@ -88,7 +95,7 @@ services = [
 # 数据库服务
 mysql_service = {
     'images': "mysql:5.7.37",
-    'service_name': 'hypref_mall_mysql',
+    'service_name': 'hyperf_mall_mysql',
     'port': 5567,
     'local_path': base_dir + '/mysql/data',
     'service_root': '/var/lib/mysql',
@@ -97,15 +104,15 @@ mysql_service = {
 # redis服务
 redis_service = {
     'images': "redis",
-    'service_name': 'hypref_mall_redis',
+    'service_name': 'hyperf_mall_redis',
     'port': 5568
 }
 
 # web-admin 后台
 web_admin = {
     'git_name': "hypref_mall_font",
-    'images': "hypref_mall_font",
-    'service_name': 'hypref_mall_font',
+    'images': "hyperf_mall_font",
+    'service_name': 'hyperf_mall_font',
     'port': 5569
 }
 
--- install/main.py
+++ install/main.py
@@ -63,7 +63,7 @@ def reload_web_admin():
 
 
 def reload_service(name):
-    if name == 'hypref_mall_font':
+    if name == 'hyperf_mall_font':
         reload_web_admin()
     else:
         try:
@@ -85,29 +85,30 @@ def start_container():
                + string +
                "\n\n-----------------------------\n\n"
                "    执行Hyperf命令：服务编号+1\n"
-               "    特殊命令 99->一键安装\n\n"
+               "    99：一键安装\n\n"
                "-----------------------------\n "
                "请输入要执行的命令：")
 
-    if cm not in service_index:
+    if cm == '99':
+        nnn = input("确认开始初始化,Yes / No \n")
+        if nnn == 'Yes' or nnn == 'yes':
+            initProject.init()
+        else:
+            pass
+
+    elif cm not in service_index:
         if cm in cmd_service:
             c = cm[0:1]
             mmm = input("输入命令,如：lazy:get config\n")
-            if serviceName[int(c)] == 'hypref_mall_font':
+            if serviceName[int(c)] == 'hyperf_mall_font':
                 print("\nweb_admin不支持该命令\n")
             else:
                 os.system('docker exec ' + serviceName[int(c)] + ' php ' + config.service_project_dir
                           + '/bin/hyperf.php ' + mmm)
         else:
             print("\n命令未识别，请输入正确的命令\n")
-            start_container()
-    elif cm == '99':
-        nnn = input("确认开始初始化,Yes / No \n")
-        if nnn == 'Yes' or nnn == 'yes':
-            initProject.init()
-        else:
-            pass
-        # start_container()
+
+        start_container()
     else:
         name = serviceName[int(cm)]
         if name == 'all':
@@ -116,7 +117,8 @@ def start_container():
                     task = threading.Thread(target=reload_service, args=(container_name,))
                     task.start()
         else:
-            reload_service(name)
+            task = threading.Thread(target=reload_service, args=(name,))
+            task.start()
     start_container()
 
 
